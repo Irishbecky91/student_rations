78c5f54564f84b481d21122af05be6b5678f79e2
Rebecca Rayner <rebeccacains1991@hotmail.com>
2022-03-10 21:29:33 +0000

    Replace PostgreSQL url and install crispy forms

--- student_rations/settings.py
+++ student_rations/settings.py
@@ -45,7 +45,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'recipes',
+    'crispy_forms',
+    "crispy_bootstrap5",
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
